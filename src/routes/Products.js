ba5fe504059879ffc17d1e4986afb7164c5ce0c5
ELEVEN <lov.2014@gmail.com>
2016-10-09 15:59:46 +0800

    init

---
+++ src/routes/Products.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { connect } from 'dva'
+import ProductList from '../components/ProductList'
+
+const Products = (props) => {
+  function handleDelete(id) {
+    props.dispatch({
+      type: 'products/delete',
+      payload: id,
+    })
+  }
+
+  return (
+    <div>
+      <h2>List of ProductList</h2>
+      <ProductList onDelete={handleDelete} products={props.products} />
+    </div>
+  )
+}
+
+export default connect(({ products }) => ({
+  products,
+}))(Products)
